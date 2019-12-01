27a8a735f4af43e17737a03c9527e17e80d70138
Nadav Bar David <bardanad@post.bgu.ac.il>
2019-12-01 12:26:19 +0200

    Few fixes

--- src/main/java/Script.java
+++ src/main/java/Script.java
@@ -1,24 +1,26 @@
+import javafx.util.Pair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 
 public class Script {
     private HashSet<String> names;
     private TreeMap<String, Integer> substringToFreq;
     private HashSet<String>[] lengthToSubstrings;
+    private FileWriter fw;
+    private BufferedWriter bw;
     private char[][] freqPrevChar;
     private char[] firstLetterFreq;
     private final int LONGEST_NAME = 500;
-    private final int LENGTH_OF_AUTO_GENERATED_NAME = 5;
+    private final int LENGTH_OF_AUTO_GENERATED_NAME = 6;
 
 
     private Script() {
@@ -28,9 +30,19 @@ public class Script {
         /* The next parameters are to support auto-name creation functionality. */
         freqPrevChar = new char[26][26];
         firstLetterFreq = new char[26];
+        try {
+            PrintWriter write = new PrintWriter("fullLog.txt");
+            write.print("");
+            write.close();
+            fw = new FileWriter("fullLog.txt");
+            bw = new BufferedWriter(fw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private int countSpecificString(String string) {
+    private int CountSpecificString(String string) {
+        System.out.println(substringToFreq.getOrDefault(string, 0));
         return (substringToFreq.getOrDefault(string, 0));
     }
 
@@ -38,8 +50,21 @@ public class Script {
         if (lengthToSubstrings[n] == null)
             return;
         HashSet strings = lengthToSubstrings[n];
-        for (Object s : strings) {
-            System.out.println(s + ":" + n);
+        System.out.println("Results <<<<<<<CountAllStrings with n = " + n + ">>>>>>>");
+        try {
+            bw.write("Results <<<<<<<CountAllStrings with n = " + n + ">>>>>>>" + "\n");
+            TreeSet sortedStrings = new TreeSet<>(strings);
+            for (Object s : sortedStrings) {
+                int freq = substringToFreq.get(s.toString());
+                bw.write(s + ":" + freq + "\n");
+            }
+            bw.flush();
+            System.out.println("Total <<<<<<<CountAllStrings with n = " + n + " Total substrings: " + strings.size() + " SEE FULL RESULTS ON fullLog.txt>>>>>>>");
+            bw.write("Total <<<<<<<CountAllStrings with n = " + n + " Total substrings: " + strings.size() + ">>>>>>>");
+            System.out.println("---------------------------------------------------------");
+            bw.write("---------------------------------------------------------");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -50,15 +75,16 @@ public class Script {
         freqToSubstrings = new TreeMap<>();
 
         for (String substring : substrings) {
-            substring = substring.toLowerCase();
             int freq = 0;
-
-            /* Ignoring case-sensitive issue, We sum the frequency*/
             if (substringToFreq.containsKey(substring))
                 freq += substringToFreq.get(substring);
-            if (substringToFreq.containsKey(substring.toUpperCase()))
-                freq += substringToFreq.get(substring.toUpperCase());
+            /* Ignoring case-sensitive issue, We sum the frequency*/
+            if (Character.isUpperCase(substring.charAt(0))) {
+                substring = substring.toLowerCase();
+                if (substringToFreq.containsKey(substring))
+                    freq += substringToFreq.get(substring);
 
+            }
             /* Checks if the specific freq already has an entry on data structure */
             if (freqToSubstrings.containsKey(freq)) {
                 HashSet<String> oldSubstrings = freqToSubstrings.get(freq);
@@ -73,15 +99,18 @@ public class Script {
 
         /* Polls last entry -> maximum freq value */
         HashSet<String> res = freqToSubstrings.pollLastEntry().getValue();
+        System.out.println("Results <<<<<<<CountMaxString with n = " + n + ">>>>>>>");
         for (String subString : res) {
             System.out.println(subString);
         }
+        System.out.println("---------------------------------------------------------");
         return res;
     }
 
     private ArrayList<String> AllIncludesString(String string) {
         ArrayList<String> res = new ArrayList<>();
         string = string.toLowerCase();
+        System.out.println("Results <<<<<<<AllIncludesString with String = " + string + ">>>>>>>");
         for (String name : names) {
             name = name.toLowerCase();
             if (string.contains(name)) {
@@ -89,32 +118,51 @@ public class Script {
                 res.add(name);
             }
         }
+        System.out.println("---------------------------------------------------------");
         return res;
     }
 
     private String GenerateName() {
         StringBuilder sb;
         sb = new StringBuilder();
-        char currentChar = getFrequentChar(firstLetterFreq);
+        char currentChar = getFrequentChar(firstLetterFreq).getKey();
         sb.append(Character.toUpperCase(currentChar));
-
         for (int i = 1; i < LENGTH_OF_AUTO_GENERATED_NAME; i++) {
-            char nextChar = getCharByPrevFreq(currentChar);
+            char nextChar = getCharByPrevFreq(Character.toLowerCase(currentChar));
             sb.append(nextChar);
             currentChar = nextChar;
         }
-        return null;
+        System.out.println("Results <<<<<<<GenerateName with length = " + LENGTH_OF_AUTO_GENERATED_NAME + ">>>>>>>");
+        System.out.println(sb.toString());
+        System.out.println("---------------------------------------------------------");
+        return sb.toString();
     }
 
-    private char getCharByPrevFreq(char prefChar) {
-        prefChar = Character.toLowerCase(prefChar);
-        return 't';
+    private char getCharByPrevFreq(char prevChar) {
+        prevChar = Character.toLowerCase(prevChar);
+        int max = Integer.MIN_VALUE;
+        char c = 0;
+        for (int i = 0; i < 26; i++) {
+            char[] charArray = freqPrevChar[i];
+            if (charArray[prevChar - 97] > max) {
+                max = charArray[prevChar - 97];
+                c = (char) (i + 97);
+            }
+        }
+        return c;
     }
 
-    private char getFrequentChar(char[] charArray) {
-        return 't';
+    private Pair<Character, Integer> getFrequentChar(char[] charArray) {
+        int max = Integer.MIN_VALUE;
+        char c = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] > max) {
+                max = charArray[i];
+                c = ((char) (i + 97));
+            }
+        }
+        return new Pair(c, max);
     }
-
 
     private void parseSite() {
         try {
@@ -159,11 +207,14 @@ public class Script {
 
     private void addName(String name) {
         if (!names.contains(name)) { // Prevent duplication
-            subString(name);
-            names.add(name);
+            String[] splitedNames = name.split(" ");
+            for (int i = 0; i < splitedNames.length; i++) {
+                subString(splitedNames[i]);
+                names.add(splitedNames[i]);
+            }
         }
-
     }
+
 
     // Function to print all sub strings
     private void subString(String name) {
@@ -173,6 +224,16 @@ public class Script {
             freqPrevChar[str[1] - 97][Character.toLowerCase(str[0]) - 97] += 1;
         }
         for (int i = 2; i < str.length; i++) {
+            /*
+            If we recognize a name that consists of two words, we chose to define the letter before the space as the letter before the word after the space.
+            e.g name: John Paul
+            * */
+            if (str[i] == 32) {
+                str[i + 1] = Character.toLowerCase(str[i + 1]);
+                freqPrevChar[str[i + 1] - 97][str[i - 1] - 97] += 1;
+                i++;
+                continue;
+            }
             freqPrevChar[str[i] - 97][str[i - 1] - 97] += 1;
         }
         int n = name.length();
@@ -185,7 +246,7 @@ public class Script {
                 for (int k = i; k <= j; k++) {
                     tmp += str[k];
                 }
-                System.out.println(tmp);
+//                System.out.println(tmp);
                 addToLengthToSubstrings(tmp);
                 addToSubstringsToFreq(tmp);
             }
@@ -217,6 +278,7 @@ public class Script {
 
     /**
      * The function of this function is to take the full line of the element that holds the name we ultimately want to receive, and clear it so that we only get the name.
+     *
      * @param nameElement - The element which holds the name we want to extract.
      * @return - The name we want to add to our data structures.
      */
@@ -237,11 +299,67 @@ public class Script {
 
     public static void main(String[] args) {
         Script s = new Script();
-        s.parseSite();
-        System.out.println(s.countSpecificString("elle"));
+        File path = new File("names.txt");
+        String name;
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            while ((name = br.readLine()) != null) {
+                s.addName(name);
+            }
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+
+//        if (args != null && args[0] != null)
+//            switch (args[0]) {
+//                case "CountSpecificString":
+//                    s.CountSpecificString((args[1]));
+//                    break;
+//                case "CountAllStrings":
+//                    s.CountAllStrings(Integer.parseInt(args[1]));
+//                    break;
+//                case "CountMaxString":
+//                    s.CountMaxString(Integer.parseInt(args[1]));
+//                    break;
+//                case "AllIncludesString":
+//                    s.AllIncludesString(args[1]);
+//                    break;
+//                case "GenerateName":
+//                    s.GenerateName();
+//                    break;
+//            }
+
+
+        /* CountSpecificString CHECKED */
+//        s.CountSpecificString("Yan"); // 1
+//        s.CountSpecificString("yan"); // 19
+//        s.CountSpecificString("ett"); // 74
+//        s.CountSpecificString("Ett"); // 2
+//        s.CountSpecificString("chr"); // 0
+//        s.CountSpecificString("Ad"); // 29
+
+
+        /* CountAllStrings CHECKED*/
 //        s.CountAllStrings(7);
-//        s.AllIncludesString("mkgddassafbadayardenfg");
-//        s.CountMaxString(4);
+//        s.CountAllStrings(8);
+
+
+
+        /* CountMaxString CHECKED */
+//        s.CountMaxString(1); // e
+//        s.CountMaxString(2); // an
+//        s.CountMaxString(3); // ell
+//        s.CountMaxString(4); // anna
+//        s.CountMaxString(5); // chris
+//        s.CountMaxString(6); // christ
+//        s.CountMaxString(7); // christi
+//        s.CountMaxString(8); // alexandr
+
+
+        /* AllIncludesString CHECKED */
+//        s.AllIncludesString("YadkbycharlieMichal");
+//        s.AllIncludesString("Egttechrisett");
+
+        /* GenerateName CHECKED */
 //        s.GenerateName();
     }
 
